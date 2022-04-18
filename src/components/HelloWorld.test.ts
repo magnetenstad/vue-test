73c05bdfaa7738e8ea89cc7a567503634b0074a3
magnetenstad <tenstad.magne@gmail.com>
2022-04-19 00:34:54 +0200

    Add eslint

--- src/components/HelloWorld.test.ts
+++ src/components/HelloWorld.test.ts
@@ -2,18 +2,16 @@ import { test, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 import HelloWorld from '../components/HelloWorld.vue'
 
-test('mount component', async() => {
-
+test('mount component', async () => {
   const wrapper = mount(HelloWorld, {
     props: {
       msg: 'Hi mom!'
-    },
+    }
   })
 
   expect(wrapper.text()).toContain('Hi mom!')
-  
+
   expect(wrapper.get('button').text()).toContain('count is: 0')
   await wrapper.get('button').trigger('click')
   expect(wrapper.get('button').text()).toContain('count is: 1')
-
 })
