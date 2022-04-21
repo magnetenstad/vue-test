c4a0f9d95fb4f6fd85fec5166cb8cd9b06af8dfc
magnetenstad <tenstad.magne@gmail.com>
2022-04-21 23:14:23 +0200

    Prettier

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,24 +1,24 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from 'vue';
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
   <main>
     <h1>{{ msg }}</h1>
-  
+
     <p>
       Recommended IDE setup:
       <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
       +
       <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
     </p>
-  
+
     <p>See <code>README.md</code> for more information.</p>
-  
+
     <p>
       <a href="https://vitejs.dev/guide/features.html" target="_blank">
         Vite Docs
@@ -26,7 +26,6 @@ const count = ref(0)
       |
       <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
     </p>
-
   </main>
 </template>
 
