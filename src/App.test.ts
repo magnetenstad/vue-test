287a69deb04be1bfb77bf1e66decc80072aad321
magnetenstad <tenstad.magne@gmail.com>
2022-04-21 20:18:14 +0200

    Add test with router

---
+++ src/App.test.ts
@@ -0,0 +1,22 @@
+import { mount } from '@vue/test-utils'
+import App from './App.vue'
+import router from './router'
+
+test('routing', async () => {
+  
+  await router.push('/')
+  await router.isReady()
+
+  const wrapper = mount(App, {
+    global:{
+      plugins: [router]
+    }
+  })
+
+  expect(wrapper.html()).toContain('Hello Vue 3 + TypeScript + Vite')
+
+  await router.push('/counter')
+  await router.isReady()
+
+  expect(wrapper.html()).toContain('Count: 0')
+})
